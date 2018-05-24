02aec81633300e26a543bc9966b76a9161b9e7bf
agatak8 <shnutzer@gmail.com>
2018-05-24 20:16:19 +0200

    change Main class to extend App

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,14 +2,12 @@ import server.{FileConfig, WebServer}
 import service.{LoginService, ParkingService}
 import database.MockDatabase
 
-object Main {
+object Main extends App {
   implicit val database = MockDatabase
   implicit val loginService = new LoginService()
   implicit val parkingService = new ParkingService()
   implicit val config = FileConfig
   val server = new WebServer()
 
-  def main(args: Array[String]): Unit = {
-    server.start()
-  }
+  server.start()
 }
