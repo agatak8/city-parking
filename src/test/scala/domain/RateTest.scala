3dbfa5b46dfc3b6dd3258d856e85733783ea8278
agatak8 <shnutzer@gmail.com>
2018-05-25 14:59:48 +0200

    refactor Rate test to get rid of hardcoded calculated values

--- src/test/scala/domain/RateTest.scala
+++ src/test/scala/domain/RateTest.scala
@@ -1,32 +1,33 @@
 package domain
 
-import org.scalatest.FlatSpec
+import org.scalatest.{FlatSpec, Matchers}
 
-class RateTest extends FlatSpec {
+class RateTest extends FlatSpec with Matchers {
   behavior of "Rates"
   it should "return a rate for the PLN currency" in {
-    assert(Rates.getRate(Currencies.PLN).isInstanceOf[Rate])
+    Rates.getRate(Currencies.PLN) shouldBe a [Rate]
   }
   behavior of "PLN regular rate"
   it should "cost 1 PLN for the 1st hour" in {
     val rate = Rates.getRate(Currencies.PLN)
-    assert(rate.getRegular(0) == 1)
+    rate.getRegular(0) shouldBe 1
   }
 
   it should "cost 2 PLN for the 2nd hour" in {
     val rate = Rates.getRate(Currencies.PLN)
-    assert(rate.getRegular(1) == 2)
+    rate.getRegular(1) shouldBe 2
   }
 
   it should "cost 1.5x more than the previous one for the 3rd and next hour" in {
     val rate = Rates.getRate(Currencies.PLN)
-    val expectedValues = Range(1, 5).map {
+    def expectedRate(hour: Int): BigDecimal = hour match {
       case 1 => BigDecimal("2")
-      case 2 => BigDecimal("3")
-      case 3 => BigDecimal("4.5")
-      case 4 => BigDecimal("6.75")
+      case other => expectedRate(other - 1) * BigDecimal("1.5")
     }
-    assert(Range(1, 5).map(hour => rate.getRegular(hour)) == expectedValues)
+    def expectedValues = Range(2, 8).map(hour => expectedRate(hour))
+    def actualValues = Range(2, 8).map(hour => rate.getRegular(hour))
+
+    actualValues shouldBe expectedValues
   }
 
   behavior of "PLN VIP rate"
@@ -42,12 +43,13 @@ class RateTest extends FlatSpec {
 
   it should "cost 1.2x more than the previous one for the 3rd and next hour" in {
     val rate = Rates.getRate(Currencies.PLN)
-    val expectedValues = Range(1, 5).map {
+    def expectedRate(hour: Int): BigDecimal = hour match {
       case 1 => BigDecimal("2")
-      case 2 => BigDecimal("2.4")
-      case 3 => BigDecimal("2.88")
-      case 4 => BigDecimal("3.456")
+      case other => expectedRate(other - 1) * BigDecimal("1.2")
     }
-    assert(Range(1, 5).map(hour => rate.getVIP(hour)) == expectedValues)
+    def expectedValues = Range(2, 8).map(hour => expectedRate(hour))
+    def actualValues = Range(2, 8).map(hour => rate.getVIP(hour))
+
+    actualValues shouldBe expectedValues
   }
 }
